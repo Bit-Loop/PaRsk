01e3208c526dfdd56e6c345b652743dcbdc05353
Isaiah Jackson <bitloop@mail.byteloop.net>
2021-11-12 09:52:51 +0000

    Update emulator/source/modules/rv32i.h, emulator/source/modules/rv32i.c, emulator/source/modules/controller.c, emulator/source/modules/controller.h files

---
+++ emulator/source/modules/controller.h
@@ -0,0 +1,14 @@
+#ifndef CONTROLLER_H // if HASHTABLE_H is not defined 
+#define CONTROLLER_H // then define it here.
+#ifdef __cplusplus //check if using a C++ compiler
+extern "C" { // if true ^ then use C naming rules for C++ compilation
+#endif
+
+	
+// code here
+
+	
+#ifdef __cplusplus // if true exit the extern c block
+}
+#endif
+#endif
---
+++ emulator/source/modules/rv32i.h
@@ -0,0 +1,14 @@
+#ifndef RV32I_H // if HASHTABLE_H is not defined 
+#define RV32I_H // then define it here.
+#ifdef __cplusplus //check if using a C++ compiler
+extern "C" { // if true ^ then use C naming rules for C++ compilation
+#endif
+
+	
+// code here
+
+	
+#ifdef __cplusplus // if true exit the extern c block
+}
+#endif
+#endif
