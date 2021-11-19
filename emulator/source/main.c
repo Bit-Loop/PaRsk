efc6433b2dce342c574e6ea98c939ec6ec8af68b
Isaiah Tyler Jackson <isaiah.fpga@gmail.com>
2021-11-18 21:20:16 -0600

    Added <stdint.h> to main.c
    Attempt to fix "error: unknown type name 'uint32_t'"

--- emulator/source/main.c
+++ emulator/source/main.c
@@ -7,7 +7,7 @@
 
 
 #include <stdio.h>
-
+#include <stdint.h>
 
 int main (){
 
--- emulator/source/modules/config.h
+++ emulator/source/modules/config.h
@@ -3,7 +3,7 @@
 #ifdef __cplusplus //check if using a C++ compiler
 extern "C" { // if true ^ then use C naming rules for C++ compilation
 #endif
-
+	
 	#define regWidth uint32_t
 	
 #ifdef __cplusplus // if true exit the extern c block
